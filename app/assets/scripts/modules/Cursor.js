07ea737ecf4389d91643e5698856b2f5fca97449
Olivier Rasquin <rasquin.olivier@gmail.com>
2020-12-31 17:30:39 +0100

    progress on overflow detection

--- app/assets/scripts/modules/Cursor.js
+++ app/assets/scripts/modules/Cursor.js
@@ -5,10 +5,20 @@ const Cursor = () => {
   const cursor = document.querySelector(".cursor");
   const links = document.querySelectorAll("[data-hover]");
   const taskList = document.querySelector(".taskcard__container");
+
   let isLinkHover = false;
   let isTextInputHover = false;
   let isScrolling = false;
+  let isOverflowing = false;
   let currentHoveredLink;
+  // init cursor's infos to use in multiple functions
+  let cursorInfos = {
+    cursorHeight: 25,
+    cursorWidth: 25,
+    cursorOpacity: 0.15,
+    cursorRadius: "50%",
+    cursorTransition: 0.2,
+  };
   // init link's infos to use in multiple functions
   let linkInfo = {
     linkWidth: 0,
@@ -20,14 +30,15 @@ const Cursor = () => {
 
   const followMouse = (e) => {
     // values change if hovering a text input
-    let cursorHeight = isTextInputHover ? 23 : 25;
-    let cursorWidth = isTextInputHover ? 2 : 25;
-    let cursorOpacity = isTextInputHover ? 0.25 : 0.15;
-    let cursorRadius = isTextInputHover ? 0 : "50%";
+    // let cursorHeight = isTextInputHover ? 23 : 25;
+    // let cursorWidth = isTextInputHover ? 2 : 25;
+    // let cursorOpacity = isTextInputHover ? 0.25 : 0.15;
+    // let cursorRadius = isTextInputHover ? 0 : "50%";
     // if mouse come back to viewport after having left it
     // disable anim duration to avoid cursor to 'run'
     // from viewport leaving point to entering point
-    let transitionDuration = cursor.style.visibility == "hidden" ? 0 : 0.2;
+    // let transitionDuration = cursor.style.visibility == "hidden" ? 0 : 0.2;
+    defaultRoundCursor();
     // make the cursor visible again
     cursor.style.visibility = "visible";
     // disable mouse follow if hovering a link
@@ -35,15 +46,29 @@ const Cursor = () => {
       // check coordinates of mouse
       // and apply to cursor element 'top' and 'left' properties
       gsap.to(cursor, {
-        duration: transitionDuration,
+        duration: cursorInfos.cursorTransition,
         x: e.pageX,
         y: e.pageY,
-        height: cursorHeight,
-        width: cursorWidth,
-        opacity: cursorOpacity,
-        borderRadius: cursorRadius,
+        height: cursorInfos.cursorHeight,
+        width: cursorInfos.cursorWidth,
+        opacity: cursorInfos.cursorOpacity,
+        borderRadius: cursorInfos.cursorRadius,
       });
     }
+  };
+
+  const defaultRoundCursor = () => {
+    // values change if hovering a text input
+    cursorInfos.cursorHeight = isTextInputHover ? 23 : 25;
+    cursorInfos.cursorWidth = isTextInputHover ? 2 : 25;
+    cursorInfos.cursorOpacity = isTextInputHover ? 0.25 : 0.15;
+    cursorInfos.cursorRadius = isTextInputHover ? 0 : "50%";
+    // if mouse come back to viewport after having left it
+    // disable anim duration to avoid cursor to 'run'
+    // from viewport leaving point to entering point
+    cursorInfos.cursorTransition = cursor.style.visibility == "hidden" ? 0 : 0.2;
+    // make the cursor visible again
+    cursor.style.visibility = "visible";
   };
 
   const getLinkInfo = (el) => {
@@ -62,17 +87,20 @@ const Cursor = () => {
   };
 
   const linkHover = (e) => {
+    // assign current link hovered (useful in 'translateHoveredElement' function)
     currentHoveredLink = e.target;
+    // get hovered link's infos
     getLinkInfo(e.target);
-
-    // do not scale & freeze cursor if hovering text input
+    // do not 'scale & freeze' cursor when hovering text input
     if (e.target.nodeName == "INPUT") {
       isTextInputHover = true;
       isLinkHover = false;
-    } else {
+    } else if (!checkOverflow()) {
+      console.log("ok");
       isLinkHover = true;
       // change cursor size with hovered link values
       changeCursorSize(linkInfo);
+    } else {
     }
   };
 
@@ -82,7 +110,7 @@ const Cursor = () => {
     let yPosition = link.linkTop + link.linkHeight / 2;
     let xPosition = link.linkLeft + link.linkWidth / 2;
     // if
-    if (isLinkHover) {
+    if (isLinkHover & !checkOverflow()) {
       gsap.to(cursor, {
         duration: 0.25,
         x: xPosition,
@@ -99,6 +127,7 @@ const Cursor = () => {
   const releaseCursor = (e) => {
     isLinkHover = false;
     isTextInputHover = false;
+    isOverflowing = false;
   };
 
   const hideCursor = (e) => {
@@ -114,23 +143,43 @@ const Cursor = () => {
     }
   };
 
+  const checkOverflow = () => {
+    // get size & position of tasks list
+    let taskListTop = taskList.getBoundingClientRect().y;
+    // if element top value is smaller than task list top value
+    // means that the element is overflowing
+    if (linkInfo.linkTop < taskListTop) {
+      isOverflowing = true;
+      return true;
+    }
+  };
+
   const translateHoveredElement = (e) => {
+    if (checkOverflow()) {
+      isOverflowing = true;
+      isLinkHover = false;
+    }
+    // get size & position of tasks list
+    // let taskListTop = e.target.getBoundingClientRect().y;
+    // if element top value is smaller than task list top value
+    // means that the element is overflowing
+    // if (linkInfo.linkTop < taskListTop) {
+    //   isOverflowing = true;
+    //   isLinkHover = false;
+    //   defaultRoundCursor();
+    // }
+    // update link infos
     getLinkInfo(currentHoveredLink);
-    // get task list scrolling position
-    let scrollPosition = e.target.scrollTop;
-    // ... and remove this value to the cursor position
+    // gsap put the 'transform-origin' to 'center
+    // need to add half the size of the element to the position values
     let yPosition = linkInfo.linkTop + linkInfo.linkHeight / 2;
 
-    gsap.to(cursor, {
-      duration: 0,
-      y: yPosition,
-    });
-    // console.table(
-    //   "scroll top:" + e.target.scrollTop,
-    //   ", Yposition" + linkInfo.linkTop,
-    //   ", New YPosition:" + yPosition,
-    //   linkInfo
-    // );
+    if (!isOverflowing) {
+      gsap.to(cursor, {
+        duration: 0,
+        y: yPosition,
+      });
+    }
   };
 
   // Event Listener - mouse moving
@@ -146,7 +195,7 @@ const Cursor = () => {
   links.forEach((link) => {
     link.addEventListener("mouseleave", releaseCursor);
   });
-  // Event Listener - scroll task list
+  // Event Listener - scroll on tasks list
   taskList.addEventListener("scroll", translateHoveredElement);
 };
 
